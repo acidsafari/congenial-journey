b6156b17b362791eea012167182172bc776826e9
Samuel Klett Navarro <77330368+acidsafari@users.noreply.github.com>
2021-01-24 17:14:46 +0100

    Add files via upload
    score card elif sequence

---
+++ ex_3_2(score).py
@@ -0,0 +1,16 @@
+gradescore = input("Enter Score between 0 and 1: ")
+
+score = float(gradescore)
+
+if score < 0 and score > 1 :
+    print('Error, PLEASE ENTER VALUES BETWEEN 0 AND 1!')
+elif score >= 0.9 :
+    print('A')
+elif score >= 0.8 :
+    print('B')
+elif score >= 0.7 :
+    print('C')
+elif score >= 0.6 :
+    print('D')
+else score < 0.6 :
+    print('F')
